5de2f9ae81b59cd049db8267623effa5d5dd5fea
William957-web <85293841+William957-web@users.noreply.github.com>
2023-12-31 15:10:20 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM dockerfiles/centos-lamp 
+
+COPY index.php /var/www/html/index.php
+COPY console.php /var/www/html/console.php
+COPY login.php /var/www/html/login.php
+RUN echo 'ICED{PT_4_bab13s_have_fUn!!!}' > /flag-$(xxd -l 6 -p /dev/urandom)
---
+++ login.php
@@ -0,0 +1,72 @@
+<?php 
+if(isset($_POST['username'])&&isset($_POST['password'])){
+    if($_POST['username']!=''&&$_POST['password']!=''){
+        if($_POST['username']!='Whale'&&$_POST['password']!='R4hl1nIsTheCutiest'){
+            echo('<h1>Login Failed</h1>');
+        }
+    }
+    else{
+        setcookie("cookie", "HEYCUPERSECRETCOOKIE");
+        header('Location: console.php');
+        exit();
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Simple Login Form Example</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik:400,700"><link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<!---
+if(isset($_POST['username'])&&isset($_POST['password'])){
+    if($_POST['username']!=''&&$_POST['password']!=''){
+        if($_POST['username']!='HIDDEN'&&$_POST['password']!='HIDDEN'){
+            echo('<h1>Login Failed</h1>');
+        }
+    }
+    else{
+        header('Location: console.php');
+        echo('Welcome, administrator')
+        exit();
+    }
+}s
+--->
+<div class="login-form">
+<form action="login.php" method="post">
+<h1>Login</h1>
+<div class="content">
+<div class="input-field">
+<input style="text" name="username" id="username" placeholder="Email" autocomplete="nope">
+</div>
+<div class="input-field">
+<input type="text" name="password" id="password" placeholder="Password">
+</div>
+<h3><sapn id="Outcome"></sapn></h3>
+</div>
+<div class="action">
+<button>Register</button>
+<button id="Button"  type="submit">Sign in</button>
+</div>
+</form>
+</div>
+<script>
+  const passwd=document.querySelector('#Passwd');
+  const account=document.querySelector('#Account');
+  const Button=document.querySelector('#Button');
+  const Outcome=document.querySelector('#Outcome');
+  let headers = {
+    "Content-Type": "multipart/form-data"
+    }
+  Button.addEventListener('click',()=>{
+    fetch('./login.php', {
+    method: "POST",
+    headers: headers,
+    body: "username="+Account.value+"&password="+Passwd.value
+    })
+  })
+</script>
+</body>
+</html>
